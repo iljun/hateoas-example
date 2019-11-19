79cd7c2ab75ac9fa10c74d1b4295490efd0b1d93
iljun <sumusb@naver.com>
2019-11-19 23:09:13 +0900

    project setup

---
+++ src/main/java/com/iljun/hateoasexample/HateoasExampleApplication.java
@@ -0,0 +1,13 @@
+package com.iljun.hateoasexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HateoasExampleApplication.class, args);
+    }
+
+}
